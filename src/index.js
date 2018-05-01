9016a001eba0aecdc172735e9206e1bd2c2ecfca
QuantumlyTangled <EthoArx@gmail.com>
2018-05-01 13:50:08 +0200

    Lets get serious

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 module.exports = {
 	Master: require('./master.js'),
-	Constants: require('./lib/utilities/Constants.mjs')
+	Constants: require('./lib/utilities/Constants.mjs'),
+	version: require('../package.json').version,
+
+	Chart: require('./lib/utilities/Charts.js')
 };
---
+++ src/lib/utilities/Charts.js
@@ -0,0 +1,46 @@
+const { promisify } = require('util');
+const { writeFile } = require('fs-nextra');
+try {
+	var exporter = require('highcharts-export-server');
+	if (!exporter.initPool) exporter = null;
+} catch (err) { } // eslint-disable-line no-empty
+const render = promisify(exporter.export);
+exporter.initPool();
+
+module.exports = class Chart {
+
+	constructor() {
+		this.type = 'png';
+		this.options = {};
+		this.options.credits = {};
+		this.options.credits.enabled = false;
+		this.options.chart = {};
+		this.options.chart.backgroundColor = '#2C2F33';
+		this.options.chart.borderColor = '#3498DB';
+		this.options.chart.borderWidth = 2;
+		this.options.title = {};
+		this.options.title.style = {};
+		this.options.title.style.color = '#FFFFFF';
+		this.options.title.style.fontWeight = 'bold';
+		this.options.xAxis = {};
+		this.options.xAxis.type = 'datetime';
+		this.options.xAxis.labels = { style: { color: '#FFFFFF' } };
+		this.options.yAxis = {};
+		this.options.yAxis.title = { text: null };
+		this.options.yAxis.labels = { style: { color: '#FFFFFF' } };
+		this.options.legend = {};
+		this.options.legend.itemStyle = {};
+		this.options.legend.itemStyle.color = '#FFFFFF';
+		this.options.legend.itemStyle.fontWeight = 'bold';
+		this.options.plotOptions = {};
+		this.options.plotOptions.series = {};
+		this.options.series = [];
+	}
+
+	async generateChart(location) {
+		const res = await render(this);
+		const data = res.data.replace(/^data:image\/\w+;base64,/, '');
+		return writeFile(location, data, 'base64');
+	}
+
+};
